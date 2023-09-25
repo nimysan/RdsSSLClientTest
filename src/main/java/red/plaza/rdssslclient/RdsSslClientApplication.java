8908fab53c0b945ba43ba9942bfe7a8b5fd0adc6
nimysan <234aini@gmail.com>
2023-09-25 11:13:00 +0800

    rds ssl client

---
+++ src/main/java/red/plaza/rdssslclient/RdsSslClientApplication.java
@@ -0,0 +1,20 @@
+package red.plaza.rdssslclient;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableWebMvc
+public class RdsSslClientApplication implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdsSslClientApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("test");
+    }
+}
